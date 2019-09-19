cea5c3ac3ca75e13ec1b69bd764d7c355fd2ca9e
skrabacher <37253871+skrabacher@users.noreply.github.com>
2019-09-19 16:07:17 -0700

    Meets Expectations FINAL DRAFT - Ready for submit
    fixed pagination button formatting (forgot to camel case className)

--- js/script.js
+++ js/script.js
@@ -1,75 +1,39 @@
 /******************************************
+Sarah Krabacher
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-// For anyone on project 2 who wants to add the search feature 
-// We Bos's Vanilla Javascript course day 6 is great practice https://javascript30.com/
+//global variables
+const studentList = document.querySelector('.student-list').children; //const to query the student list from the html file
+console.log(studentList); //logs list of students to console to check that the query is working
+const studentsPerPage = 10; //const to define number of students that will be displayed per page
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-
-      Create a variable to store the student list item elements in the student list.
-      Pro Tip: Log out the variable storing the list to ensure it equals the list of 
-      li items and not the container of the li elements.
-***/
-
-const studentList = document.querySelector('.student-list').children;
-console.log(studentList);
-const studentsPerPage = 10
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+//showPage function hides entire list of students except for one set of 10(or whatever number the studentsPerPage variable is defined as)
 const showPage = (list, page) => {
    let startList = (page * studentsPerPage) - studentsPerPage; //subtracted "studentsPerPage" instead of "10", this makes code easier to modify if you want to quickly change display per page to a number other than 10
    let endList = (page * studentsPerPage) - 1;
-   for (let i = 0; i < list.length; i += 1) {
+   for (let i = 0; i < list.length; i += 1) { //for loop iterates through the student list in sets of 10 (or whatever number the studentsPerPage const is defined as)
       if (i >= startList
       && i <= endList) {
-         list[i].style.display = 'block';
+         list[i].style.display = 'block'; 
       }
       else {
          list[i].style.display = 'none';
       }
-      
-
    }
-   console.log('I am in show page');
+   console.log('I am in show page');//logs to console everytime showPage is run (used for troubleshooting the code)
 }
-// showPage(studentList, 1); //no need to hard code this in, showpage can be called
+// showPage(studentList, 1); //no need to hard code this in, showpage can be called in the for loop that generates the li and a tags
 
-// /*** 
-//    Create the `appendPageLinks function` to generate, append, and add 
-//    functionality to the pagination buttons.
-// ***/
 
+//appendPageLinks function creates pagination buttons and uses eventlistener to make the buttons respond to user input
 const appendPageLinks = (list) => {
    const numberOfPages = Math.ceil(list.length/studentsPerPage); //calculates # of pages needed and uses Math.Ceil to round up to whole number 
    
    const div = document.createElement('div'); //creates div node
-   div.classname = ('pagination'); // adds pagination class name to div node
-   document.querySelector('.page').appendChild(div); //??? received "Uncaught TypeError" alert when using "'.pagination'" instead of "div". WHY? need to research. ANSWER: appendChild Method uses variables only? Can not figure out how to append all nodes of certain class. Will have to rely on just DIV for now :/ but I don't like this cause other divs might be accessed than the ones i intend.
+   div.className = ('pagination'); // adds pagination class name to div node
+   document.querySelector('.page').appendChild(div); // appends div node to 
    const ul = document.createElement('ul'); //creates ul node
    div.appendChild(ul); //defines ul as a child node of div node
    
@@ -91,23 +55,20 @@ const appendPageLinks = (list) => {
    let li = ul.firstElementChild;
    let a = document.getElementsByTagName('a');
    ul.addEventListener('click', (event) => {
-      for (let i = 0; i < numberOfPages; i += 1) { //removes active class from all a nodes (should just be first a node per the for loop above)
+      for (let i = 0; i < numberOfPages; i += 1) { //loop created to remove active class from all a nodes (should just be first a node per the for loop above)
          let button = a[i]; 
          if (button.className == 'active') { 
-            button.classList.remove('active');
-            console.log(button.className);
-            console.log(a); // to test that active class has been removed from all buttons
+            button.classList.remove('active');//removes active class if a button has active class
+            console.log(button.className); //to check that active class has been removed from all buttons            
          }
       }
 
       
       event.target.className = 'active'; //adds active class to the node that was clicked  
       let pageNumber = document.querySelector('.active'); //variable to pass as page number parameter to the showpage function
-      console.log(pageNumber.textContent);
+      console.log(pageNumber.textContent); //to check that textContent is returning as intended (should be numerical string corresponding to the button clicked on)
       console.log(typeof parseInt(pageNumber.textContent)); // uses typeof method to ensure string is being converted to a number using parseItn
       showPage(list, parseInt(pageNumber.textContent));  //converts textContent from string to number and passes number into showPage function               
     });
 }
 appendPageLinks(studentList);
-
-// // ***Remember to delete the comments that came with this file, and replace them with your own code comments.
